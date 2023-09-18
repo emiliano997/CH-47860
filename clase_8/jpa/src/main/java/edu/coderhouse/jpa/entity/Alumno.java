24cb97b5709e4c87fa960fb9c242c186a293df93
Emiliano <emi.perez997@gmail.com>
2023-09-18 20:08:37 -0300

    [ADD] Clase 8

--- clase_8/jpa/src/main/java/edu/coderhouse/jpa/entity/Alumno.java
+++ clase_8/jpa/src/main/java/edu/coderhouse/jpa/entity/Alumno.java
@@ -1,9 +1,7 @@
 package edu.coderhouse.jpa.entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
