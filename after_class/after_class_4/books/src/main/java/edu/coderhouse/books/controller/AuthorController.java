5c9236bf46ddc48c13fb4e6f9f4d8a1bef5871ab
Emiliano <emi.perez997@gmail.com>
2023-10-06 21:23:26 -0300

    [ADD] After Class 4

--- after_class/after_class_4/books/src/main/java/edu/coderhouse/books/controller/AuthorController.java
+++ after_class/after_class_4/books/src/main/java/edu/coderhouse/books/controller/AuthorController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.ObjectIdGenerators.StringIdGenerator;
-
 import edu.coderhouse.books.entity.AuthorEntity;
 import edu.coderhouse.books.response.AuthorResponse;
 import edu.coderhouse.books.service.AuthorService;
